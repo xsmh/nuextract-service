10ebc3e06bc0a6064a49cff82298dd0752fe41b3
xsmh <sh@smh.sh>
2025-08-20 23:42:06 +0300

    Refactor

--- nu_extract.py
+++ nu_extract.py
@@ -2,37 +2,25 @@ import json
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-def predict_NuExtract(model, tokenizer, text, schema, example=["","",""]):
-    schema = json.dumps(json.loads(schema), indent=4)
-    input_llm =  "<|input|>\n### Template:\n" +  schema + "\n"
+def predict_nuextract(model, tokenizer, text, nu_schema, example=["","",""]):
+    nu_schema = json.dumps(nu_schema, indent=4)
+    input_llm =  "<|input|>\n### template:\n" +  nu_schema + "\n"
     for i in example:
       if i != "":
-          input_llm += "### Example:\n"+ json.dumps(json.loads(i), indent=4)+"\n"
+          input_llm += "### example:\n"+ json.dumps(json.loads(i), indent=4)+"\n"
     
-    input_llm +=  "### Text:\n"+text +"\n<|output|>\n"
+    input_llm +=  "### text:\n"+text +"\n<|output|>\n"
     input_ids = tokenizer(input_llm, return_tensors="pt", truncation=True, max_length=4000).to("cpu")
-
+    
     output = tokenizer.decode(model.generate(**input_ids)[0], skip_special_tokens=True)
-    return output.split("<|output|>")[1].split("<|end-output|>")[0]
+    output_str = output.split("<|output|>")[1].split("<|end-output|>")[0]
+    output_dict = json.loads(output_str)
+    return output_dict
 
 
-model = AutoModelForCausalLM.from_pretrained("numind/NuExtract-tiny", trust_remote_code=True)
-tokenizer = AutoTokenizer.from_pretrained("numind/NuExtract-tiny", trust_remote_code=True)
-
-model.to("cpu")
-
-model.eval()
-
-text = """"""
-
-schema = """{
-    "Traction": {
-        "Github Stars": "",
-        "Number of Users": "Look for total installs, sign-ups, or users",
-        "Active Users / Engagement": "Look for community platform members, daily/weekly/monthly active users and mention what platform",
-        "Revenue": "revenue, subscriptions, or paying customers",
-    }
-    }"""
-
-prediction = predict_NuExtract(model, tokenizer, text, schema, example=["","",""])
-print(prediction)
+def load_model_and_tokenizer():
+    model = AutoModelForCausalLM.from_pretrained("numind/nuextract-tiny", trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained("numind/nuextract-tiny", trust_remote_code=True)
+    model.to("cpu")
+    model.eval()
+    return model, tokenizer
